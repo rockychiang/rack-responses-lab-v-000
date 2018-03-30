ddb357f1a13db6c2ca0682f6bc633ee60dc524b2
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-30 03:57:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    if Time.now.strftime("%H %M %S")
+    if Time.hour < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+    
+    resp.finish
     
 end
